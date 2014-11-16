f02af36be3c9831d090118a5cb4c331f6449adaf
Abigail Jones <AbbyJonesDev@gmail.com>
2014-11-16 08:53:30 -0600

    Whitelist puppy params

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -69,6 +69,6 @@ class PuppiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def puppy_params
-      params[:puppy]
+      params.require(:puppy).permit(:name, :breed_id)
     end
 end
