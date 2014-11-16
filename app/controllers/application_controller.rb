f213aee5aa9713a706d1af13728530e82879a9aa
Abigail Jones <AbbyJonesDev@gmail.com>
2014-11-16 08:39:37 -0600

    Switch to null session for CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # def self.cors_allowed_actions
+  #   @cors_allowed_actions ||= []
+  # end
+ 
+  # def self.cors_allowed_actions=(arr)
+  #   @cors_allowed_actions = arr
+  # end
+ 
+  # def self.allow_cors(*methods)
+  #   self.cors_allowed_actions += methods
+  #   before_filter :cors_before_filter, :only => methods
+  #   protect_from_forgery with: :null_session, :only => methods
+  # end
+
+  protect_from_forgery with: :null_session
 end
