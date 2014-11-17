c4b0e72c8e60dcc495a77b289d487a5a7310da6c
Abigail Jones <AbbyJonesDev@gmail.com>
2014-11-16 22:20:17 -0600

    Fixing what I broke before

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -20,7 +20,7 @@ class PuppiesController < ApplicationController
     headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
     headers['Access-Control-Max-Age'] = '1728000'
-    render :text => "", :content_type => 'text/plain'
+    # render :text => "", :content_type => 'text/plain'
   end
 
   # GET /puppies
