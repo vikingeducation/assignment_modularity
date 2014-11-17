5ab4a6037b7cbc17adf948fee524d9e31f23886f
Abigail Jones <AbbyJonesDev@gmail.com>
2014-11-16 22:40:32 -0600

    Try again

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -2,24 +2,17 @@ class PuppiesController < ApplicationController
   before_action :set_puppy, only: [:show, :edit, :update, :destroy]
 
   skip_before_filter :verify_authenticity_token
-  before_filter :cors_preflight_check
-  # after_filter :cors_set_access_control_headers
 
-  # For all responses in this controller, return the CORS access control headers.
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
 
   # If this is a preflight OPTIONS request, then short-circuit the
   # request, return only the necessary headers and return an empty
   # text/plain.
-  def cors_preflight_check
+  def set_preflight_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
     headers['Access-Control-Max-Age'] = '1728000'
+    render :text => '', :content_type => 'text/plain'
   end
 
   # GET /puppies
