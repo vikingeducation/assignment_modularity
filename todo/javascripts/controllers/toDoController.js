9c0771e753a7f0412f50d287ed9ab57a4c4c7937
niconan <nick.hax@gmail.com>
2015-09-06 20:08:23 -0700

    form validations

--- todo/javascripts/controllers/toDoController.js
+++ todo/javascripts/controllers/toDoController.js
@@ -7,8 +7,15 @@ toDo.controller('toDoController', ['$scope',
                                         dueDate: new Date(),
                                         completed: false } ];
 
-    $scope.myToDo = { text: "Get groceries from the store",
-                      dueDate: new Date(),
-                      completed: false };
+    $scope.createTask = function() {
+      var newRes = { text: $scope.taskName,
+                     dueDate: $scope.taskDeadline,
+                     completed: false };
+      $scope.toDos.push(newRes);
+      $scope.taskName = "";
+      $scope.taskDeadline = new Date();
+    };
+
+    $scope.taskDeadline = new Date();
 
   }]);
