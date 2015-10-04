81335a464a2f61c2e553c0cc4659309b99c2403a
Adam <adam.kinson@gmail.com>
2015-10-04 12:27:02 -0400

    build basic autocomplete element

---
+++ js/directives/breeds.js
@@ -0,0 +1,10 @@
+puppyShelter.directive('breeds', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/directives/breeds.html',
+    scope: {
+      allBreeds: '=',
+      query: '='
+    }
+  }
+})
