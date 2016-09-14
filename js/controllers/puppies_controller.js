b6c4a7b634303ce63294251d354426005ff0c52d
Steven Chang <prime_pork@hotmail.com>
2016-09-14 11:30:47 +1000

    got rid of the select and options for breeds and now has been replaced with a text input which when typed in shows user the different available breeds

--- js/controllers/puppies_controller.js
+++ js/controllers/puppies_controller.js
@@ -1,5 +1,6 @@
 Puppies.controller('PuppiesController', ['$scope', 'breedsService', 'puppiesService', function($scope, breedsService, puppiesService){
 
+	$scope.breedFilter = "";
 	$scope.breeds = [];
 	$scope.newPuppy = {name: "", breed_id: ""};
 	$scope.orderKey = "+name";
@@ -7,17 +8,9 @@ Puppies.controller('PuppiesController', ['$scope', 'breedsService', 'puppiesServ
 
 	$scope.deletePuppy = function(puppyId){
 		puppiesService.deletePuppy(puppyId).then(
-			function(response){removePuppy(puppyId)},
+			function(response){_removePuppy(puppyId)},
 			function(response){console.log(response)}
 		);
-	};
-
-	var removePuppy = function(puppyId){
-		for(var i = $scope.puppies.length - 1; i >= 0; i--){
-			if ($scope.puppies[i].id === puppyId){
-				$scope.puppies.splice(i, 1);
-			};
-		};
 	};
 
 	$scope.getBreeds = function(){
@@ -34,13 +27,24 @@ Puppies.controller('PuppiesController', ['$scope', 'breedsService', 'puppiesServ
 		}, function(response){});
 	};
 
+	// This isn't working right now but have contacted Viking Code School on proper instructions for sending post requests for new breeds.
+	$scope.postBreed = function(){
+		var newBreed = {name: $scope.breedFilter};
+
+		breedsService.postBreed(JSON.stringify(newBreed)).then(
+			function(response){console.log(response)
+				_ajaxPostPuppy()},
+			function(response){console.log(response)}
+		);
+	};
+
 	$scope.postPuppy = function(){
-		puppiesService.postPuppy(JSON.stringify($scope.newPuppy)).then(function(response){
-				$scope.newPuppy = {name: "", breed_id: ""};
-				$scope.puppies.push({id: response.data.id, 
-														 breed: {name: _returnBreedNameById(response.data.breed_id)}, 
-														 name: response.data.name}) 
-			}, function(response){});
+		if (_returnBreedIdByName($scope.breedFilter)){
+			$scope.newPuppy.breed_id = _returnBreedIdByName($scope.breedFilter);
+			_ajaxPostPuppy();
+		} else {
+			$scope.postBreed();
+		};
 	};
 
 	$scope.setOrderKey = function(key){
@@ -55,10 +59,52 @@ Puppies.controller('PuppiesController', ['$scope', 'breedsService', 'puppiesServ
 		};
 	};
 
+	$scope.selectBreedFilter = function(name){
+		_updateBreedFilter(name);
+		_hideBreedFilter();
+	};
+
+	$scope.showBreedFilter = function(){
+		$("#breed-list").show();
+	};
+
+	var _ajaxPostPuppy = function(){
+		puppiesService.postPuppy(JSON.stringify($scope.newPuppy)).then(function(response){
+				$scope.newPuppy = {name: "", breed_id: ""};
+				$scope.puppies.push({id: response.data.id, 
+														 breed: {name: _returnBreedNameById(response.data.breed_id)}, 
+														 name: response.data.name}) 
+			}, function(response){});
+	};
+
+	var _hideBreedFilter = function(){
+		$("#breed-list").toggle();
+	};
+
+	var _updateBreedFilter = function(name){
+		$scope.breedFilter = name;
+	};
+
+	var	_returnBreedIdByName = function( name ){
+		for (var i = 0; i < $scope.breeds.length; i++){
+			if ($scope.breeds[i].name === name){
+				return $scope.breeds[i].id;
+			};
+		};
+	};
+
 	var _returnBreedNameById = function(breedId){
 		for (var i = 0; i < $scope.breeds.length; i++){
 			if ($scope.breeds[i].id === breedId){
 				return $scope.breeds[i].name;
+			};
+		};
+	};
+
+	var _removePuppy = function(puppyId){
+		for(var i = $scope.puppies.length - 1; i >= 0; i--){
+			if ($scope.puppies[i].id === puppyId){
+				$scope.puppies.splice(i, 1);
 			};
 		};
 	};
--- js/services/breeds.js
+++ js/services/breeds.js
@@ -6,5 +6,10 @@ Puppies.factory('breedsService', ['$http', function($http){
 		return $http.get('https://ajax-puppies.herokuapp.com/breeds.json');
 	};
 
+	// This isn't working right now but have contacted Viking Code School on proper instructions for sending post requests for new breeds.
+	object.postBreed = function(newBreed){
+		return $http.post('https://ajax-puppies.herokuapp.com/breeds.json', newBreed);
+	};
+
 	return object;
 }]);
