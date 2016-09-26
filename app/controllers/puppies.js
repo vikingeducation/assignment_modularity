6a4515ac85eb18449cd24444bccacecd2bbf8b1e
Mike Lee <asackofwheat@gmail.com>
2016-09-26 19:08:59 -0400

    updating list

--- app/controllers/puppies.js
+++ app/controllers/puppies.js
@@ -2,12 +2,18 @@ puppiesApp.controller('PuppiesCtrl', [
     '$scope',
     'puppies',
     'breeds',
-    function($scope, puppies, breeds) {
+    '_',
+    function($scope, puppies, breeds, _) {
+
+      $scope.puppies = [];
 
         $scope.refreshPuppies = function() {
-            puppies.getPuppies.then(function(result) {
-                $scope.puppies = result.data;
-            });
+          puppies.getPuppies.then(function(result) {
+              $scope.puppies.splice(0,$scope.puppies.length)
+              for (var i = 0; i < result.data.length; i++) {
+                $scope.puppies.push(result.data[i]);
+              }
+          });
         }
 
         breeds.getBreeds.then(function(result) {
@@ -17,9 +23,11 @@ puppiesApp.controller('PuppiesCtrl', [
         $scope.addPuppy = function() {
             var name = $scope.newPuppy.name;
             var breed_id = $scope.newPuppy.breed_id;
-            puppies.addPuppy(name, breed_id).then(function(){
-              debugger
-              $scope.refreshPuppies();
+            puppies.addPuppy(name, breed_id).then(function(object){
+              var breed = _.find($scope.breeds, {"id": object.data.breed_id})
+              object.data.breed = {};
+              object.data.breed.name = breed.name;
+              $scope.puppies.push(object.data);
             });
             $scope.newPuppy.name = "";
             $scope.newPuppy.breed_id = null;
@@ -27,7 +35,8 @@ puppiesApp.controller('PuppiesCtrl', [
 
         $scope.adoptPuppy = function(puppy_id) {
             puppies.deletePuppy(puppy_id).then(function() {
-                $scope.refreshPuppies;
+                var puppy = _.find($scope.puppies, {"id": puppy_id});
+                _.remove($scope.puppies, puppy);
             });
         };
 
