b0fb0387136139d535ed28b2d1cf94dd587daef7
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-26 16:50:36 -0700

    updated button sorts

--- app.js
+++ app.js
@@ -14,9 +14,21 @@ function($scope, $http, BreedService, PuppyService) {
   // Puppies
   PuppyService.all();
 
+  $scope.sortBy = "";
+  $scope.orderFlag = false;
+  $scope.flipFlag = function(){
+    $scope.orderFlag = !$scope.orderFlag;
+  };
+
+  $scope.orderType = function(input){
+    $scope.sortBy = input;
+    $scope.flipFlag();
+  };
+
   $scope.puppies = PuppyService.puppies;
   $scope.breeds = BreedService.breeds;
   $scope.adoptPuppy = PuppyService.destroy;
   $scope.createPuppy = PuppyService.create;
   $scope.refreshPuppies = PuppyService.all;
+
 }]);
