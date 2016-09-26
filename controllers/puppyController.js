817043f8cf80450a22003eb7800d478db5aa1c82
Alex Lach <alexglach@gmail.com>
2016-09-26 19:03:30 -0400

    new puppies are created

--- controllers/puppyController.js
+++ controllers/puppyController.js
@@ -1,15 +1,16 @@
 app.controller("PuppyCtrl", ["$scope", "Breeds", "Puppy", function($scope, Breeds, Puppy){
 
   $scope.breeds = Breeds.getBreedObjects();
+  $scope.puppies = Puppy.getPuppies();
 
   $scope.newPuppy = function(){
     var pup = {};
     pup.name = $scope.formData.name;
     pup.breed_id = $scope.formData.breed;
-    console.log($scope.formData);
     Puppy.createPuppy(pup);
     //var createdPuppy = Puppy.createPuppy(puppy);
     $scope.formData = {};
+    Puppy.getPuppies();
   }
   
 }])
--- services/puppy.js
+++ services/puppy.js
@@ -1,5 +1,7 @@
 app.factory("Puppy", ["$http", function($http) {
 
+  var _puppies = []
+
   var createPuppy = function(puppyObj){
     var url = "https://ajax-puppies.herokuapp.com/puppies.json"
     $http.post(url, JSON.stringify(puppyObj))
@@ -10,11 +12,35 @@ app.factory("Puppy", ["$http", function($http) {
       console.log("No puppy for you");
       console.log(data);
     })
+  };
+
+  var getPuppiesJSON = function(){
+    var url = "https://ajax-puppies.herokuapp.com/puppies.json"
+    $http.get(url)
+    .success(function(data){
+      _puppies.splice(0, _puppies.length)
+      for (var i = 0; i < data.length; i++) {
+        _puppies.push(data[i])
+      }
+    })
+    .error(function(){
+      console.log("Failed!");
+    })
+  };
+
+  var getPuppies = function(){
+    getPuppiesJSON();
+    return _puppies;
   }
 
 
+
+
+
+
   return {
-    createPuppy: createPuppy
+    createPuppy: createPuppy,
+    getPuppies: getPuppies
   }
 
 }]);
