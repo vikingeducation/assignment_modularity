765a26ebd20c19e29f167fbf633fca12e9c3c5ef
schoetlr <srlrs20020@gmail.com>
2016-09-29 14:29:46 -0400

    delete puppies but now creating doesn't display breed right away

--- javascripts/controllers/puppy_controller.js
+++ javascripts/controllers/puppy_controller.js
@@ -1,3 +1,19 @@
-puppyApp.controller("PuppiesCtrl", ['$scope', function($scope){
+puppyApp.controller("PuppiesCtrl", ['$scope', 'breedService', 'puppyService', function($scope, breedService, puppyService){
 
+  $scope.puppies = puppyService.getPuppies();
+
+  $scope.breeds = breedService.getBreeds();
+
+  $scope.createPuppy = function(){
+    console.log($scope.breedId);
+    puppyService.createPuppy($scope.name, $scope.breedId);
+
+    $scope.name = "";
+    $scope.breedId = "";
+  }
+
+  $scope.deletePuppy = function(id){
+    puppyService.deletePuppy(id);
+    
+  }
 }]);
--- javascripts/services/breed_service.js
+++ javascripts/services/breed_service.js
@@ -1,10 +1,41 @@
-puppyApp.factory('breedService', function(){
+puppyApp.factory('breedService', ['$http', function($http){
 
   var service = {};
 
+  _breeds = [];
 
+  var _getBreeds = function(){
+    
+    $http({
+      method: 'GET',
+      url: 'https://ajax-puppies.herokuapp.com/breeds.json'
+    }).then(function successCallback(response) {
+        console.log("success");
+        breeds = response.data;
+        
+        breeds.forEach(function(breedObject){
+          var breed = {};
+          breed.name = breedObject.name;
+          breed.id = breedObject.id;
+
+          _breeds.push(breed);
+        })
+
+      }, function errorCallback(response) {
+        console.log("there was an error getting breeds");
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
+    
+  }
+
+  service.getBreeds = function(){
+    _getBreeds();
+    return _breeds;
+  }
 
 
 
   return service;
-})
+}])
