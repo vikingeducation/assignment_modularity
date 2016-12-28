0a374db1e5c5d8c94c09ab33b7c8fc5b94baba09
Deepak <grc.deepak@gmail.com>
2016-12-28 00:24:52 -0800

    comments

--- js/controllers/appCtrl.js
+++ js/controllers/appCtrl.js
@@ -1,6 +1,32 @@
 app.controller('appCtrl', ['$scope', 'breedsService', 'puppiesService',
   function($scope, breedsService, puppiesService){
+    //Get Breed and Puppies
+    breedsService.getBreeds().then(function success(response) {
+		  $scope.breeds = response.data;
+	  });
     
+    puppiesService.getPuppies().then(function success(response) {
+      $scope.puppies = response.data;
+    });
+    
+    //Create Puppy
+    $scope.createPuppy = function(puppyParams) {
+      puppiesService.createPuppy(puppyParams).then(function success(response) {
+        response.data.breed = _.find($scope.breeds, function (b) { return b.id === response.data.breed_id })
+        $scope.puppies.push(response.data);
+        $scope.puppyParams = {};
+      });
+    }
+    
+    //Adopt Puppy
+    $scope.adopt = function(puppy) {
+      puppiesService.adopt(puppy).then(function success(response) {
+				var index = $scope.puppies.indexOf(puppy);
+				$scope.puppies.splice(index, 1);
+			});
+    }
+    
+    //Sort Functionality
     $scope.sortType = 'name';
     $scope.sortReverse = false;
     $scope.toggleSort = function(field) {
@@ -14,29 +40,6 @@ app.controller('appCtrl', ['$scope', 'breedsService', 'puppiesService',
       if(sortType == $scope.sortType && sortReverse == $scope.sortReverse) {
         return true;
       }
-    }
-    
-    breedsService.getBreeds().then(function success(response) {
-		  $scope.breeds = response.data;
-	  });
-    
-    puppiesService.getPuppies().then(function success(response) {
-      $scope.puppies = response.data;
-    });
-    
-    $scope.createPuppy = function(puppyParams) {
-      puppiesService.createPuppy(puppyParams).then(function success(response) {
-        response.data.breed = _.find($scope.breeds, function (b) { return b.id === response.data.breed_id })
-        $scope.puppies.push(response.data);
-        $scope.puppyParams = {};
-      });
-    }
-    
-    $scope.adopt = function(puppy) {
-      puppiesService.adopt(puppy).then(function success(response) {
-				var index = $scope.puppies.indexOf(puppy);
-				$scope.puppies.splice(index, 1);
-			});
     }
   }
 ]);
