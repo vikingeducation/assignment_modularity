72887c9bb0ecbcff94a1466ad9b8046db7968c06
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-24 17:21:49 -0500

    Imlement autocomplete

--- js/directives/puppyForm.js
+++ js/directives/puppyForm.js
@@ -4,19 +4,31 @@ puppies.directive("puppyForm",
     "puppyService",
     function(breedService, puppyService){
 
+      var createPuppy = function createPuppy(valid, puppyData){
+        if(valid){
+          puppyService.post(puppyData).then(function(){
+            puppyData.name=null;
+            puppyData.breedId=null;
+          })
+        }
+      };
+
+      var autocomplete = function autocomplete(breed, scope){
+        scope.newPuppy.breedId = breed.id;
+        scope.possibleBreeds = breed.name;
+      };
+
+      var setBreedInputValue = function setBreedInputValue(breedName){
+      };
+
       var setup = function setup(scope){
+
         breedService.breeds().then(function(breeds){
           scope.breeds = breeds;
-        })
+        });
 
-        scope.createPuppy = function createPuppy(valid, puppyData){
-          if(valid){
-            puppyService.post(puppyData).then(function(){
-              puppyData.name=null;
-              puppyData.breedId=null;
-            })
-          }
-        }
+        scope.createPuppy = createPuppy;
+        scope.autocomplete = autocomplete;
 
         scope.possibleBreeds = function possibleBreeds(breedFilter){
           scope.matches = [];
@@ -24,8 +36,8 @@ puppies.directive("puppyForm",
           for(var i = 0; i < breeds.length; i++){
             if(breeds[i].name.match(breedFilter)) scope.matches.push(breeds[i]);
           }
-          console.log(scope.matches);
-        }
+        };
+
       };
 
       return {
@@ -38,4 +50,4 @@ puppies.directive("puppyForm",
       };
 
     }
-]);
+  ]);
