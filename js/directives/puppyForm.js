da3879602f6cd3fe4f3fae0969363284d4b36fd9
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-24 18:32:48 -0500

    Refactoring

--- js/directives/puppyForm.js
+++ js/directives/puppyForm.js
@@ -2,25 +2,8 @@
 puppies.directive("puppyForm",
   ["breedService",
     "puppyService",
-    '$q',
-    function(breedService, puppyService, $q){
+    function(breedService, puppyService){
 
-      var getBreedByName = function(filter, breeds){
-        var breed;
-        for(var i = 0; i < breeds.length; i++) {
-          if(breeds[i].name.toLowerCase() === filter.toLowerCase()){
-            breed = breeds[i];
-            break;
-          }
-        }
-        if(!breed){
-          return breedService.newBreed(filter);
-        } else {
-          return $q(function(resolve, reject){
-            resolve(breed.id);
-          })
-        }
-      }
       var post = function post(scope, puppyData){
         puppyService.post(puppyData).then(function(){
           puppyData.name=null;
@@ -28,18 +11,27 @@ puppies.directive("puppyForm",
           scope.breedFilter=null;
           scope.showMatches = false;
         })
-      }
+      };
 
       var createPuppy = function createPuppy(scope, valid, puppyData){
         if(isNaN(puppyData.breedId)){
-          getBreedByName(scope.breedFilter, scope.breeds)
-          .then(function(breedId){
+          breedService.findByName(scope.breedFilter)
+            .then(function(breedId){
               puppyData.breedId = breedId
               post(scope, puppyData);
             });
-        }else if(valid){
+        } else if(valid){
           post(scope, puppyData)
         }
+      };
+
+      var possibleBreeds = function possibleBreeds(scope, breedFilter){
+        scope.matches = [];
+        var breeds = scope.breeds;
+        for(var i = 0; i < breeds.length; i++){
+          if(breeds[i].name.match(breedFilter)) scope.matches.push(breeds[i]);
+        }
+        scope.showMatches = true;
       };
 
       var autocomplete = function autocomplete(scope, breed){
@@ -64,15 +56,9 @@ puppies.directive("puppyForm",
           autocomplete(scope, breed);
         };
 
-        scope.possibleBreeds = function possibleBreeds(breedFilter){
-          scope.matches = [];
-          var breeds = scope.breeds;
-          for(var i = 0; i < breeds.length; i++){
-            if(breeds[i].name.match(breedFilter)) scope.matches.push(breeds[i]);
-          }
-          scope.showMatches = true;
+        scope.possibleBreeds = function(breedFilter){
+          possibleBreeds(scope, breedFilter);
         };
-
       };
 
       return {
--- js/services/puppies.js
+++ js/services/puppies.js
@@ -1,60 +1,57 @@
 
-puppies.factory("puppyService",
-["$http", "breedService",
-function($http, breedService){
-  var puppies = [];
-  var breeds = []
+puppies.factory("puppyService", ["$http", "breedService",
 
-  breedService.breeds().then(function(breedData){
-    breeds = breedData;
-  })
+  function($http, breedService){
 
-  var getPuppies = function getPuppies(){
-    return $http.get("https://ajax-puppies.herokuapp.com/puppies.json")
-      .then(function(resp){
-        angular.copy(resp.data, puppies);
-        return puppies
-      })
-  };
+    var puppies = [];
 
-  var _deletePuppy = function _deletePuppy(puppy){
-    var index = puppies.indexOf(puppy)
-    puppies.splice(index, 1);
-  };
+    var getPuppies = function getPuppies(){
+      return $http.get("https://ajax-puppies.herokuapp.com/puppies.json")
+        .then(function(resp){
+          angular.copy(resp.data, puppies);
+          return puppies;
+        });
+    };
 
-  var adoptPuppy = function adoptPuppy(puppy){
-    return $http({
-      method:"DELETE",
-      url: puppy.url
-    })
-      .then(function(){
+    var _deletePuppy = function _deletePuppy(puppy){
+      var index = puppies.indexOf(puppy);
+      puppies.splice(index, 1);
+    };
+
+    var adoptPuppy = function adoptPuppy(puppy){
+      return $http({
+        method:"DELETE",
+        url: puppy.url
+      }).then(function(){
         _deletePuppy(puppy);
       });
-  };
+    };
 
+    var postPuppy = function postPuppy(puppyData){
+      var puppy = {
+        name: puppyData.name,
+        breed_id: puppyData.breedId
+      };
 
-  var postPuppy = function postPuppy(puppyData){
-    var puppy = {
-      name: puppyData.name,
-      breed_id: puppyData.breedId
-    }
-    return $http({
-      method: "POST",
-      url: "https://ajax-puppies.herokuapp.com/puppies.json",
-      data: puppy
-    }).then(function(resp){
-      var puppy = resp.data, breed;
+      return $http({
+        method: "POST",
+        url: "https://ajax-puppies.herokuapp.com/puppies.json",
+        data: puppy
+      }).then(function(resp){
+        createPuppy(resp.data);
+      })
+    };
 
-      puppy.breed = breed;
+    var createPuppy = function createPuppy(puppy){
+      puppy.breed = breedService.findBreed(puppy.breed_id);
       puppy.url = "https://ajax-puppies.herokuapp.com/puppies/" + puppy.id +".json"
       puppies.unshift(puppy);
-    })
-  };
+    };
 
-  return {
-    puppies: getPuppies,
-    adopt: adoptPuppy,
-    post: postPuppy
-  };
+    return {
+      puppies: getPuppies,
+      adopt: adoptPuppy,
+      post: postPuppy
+    };
 
-}])
+  }]);
