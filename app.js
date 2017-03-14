4d69901f31e05394639aeea37eca17d1d1b4e172
tt <ttwang.303@gmail.com>
2017-03-14 05:18:14 -0400

    post and delete puppy

--- app.js
+++ app.js
@@ -8,12 +8,32 @@ puppies.controller('PuppiesCtrl', [
       $scope.breeds = object.data;
     });
 
-    puppyService.getAll().then(function(object) {
-      $scope.puppies = object.data
-    })
+
     $scope.showPuppies = function() {
       console.log(this.puppies);
     }
+    $scope.submitNewPuppy = function(name, breed) {
+      var puppy = { name: name, breed_id: breed };
+      console.log(puppy)
+      puppyService.registerNew(puppy);
+      $scope.refreshPuppies(); // new puppy doesn't show up on table
+      $scope.puppyName = "";
+      $scope.puppyBreed = "?";
+    }
+    $scope.refreshPuppies = function() {
+      console.log('refreshing')
+      puppyService.getAll().then(function(object) {
+        $scope.puppies = object.data;
+      })
+    }
+    $scope.deletePuppy = function(id) {
+      console.log('deleting' + id)
+      puppyService.delete(id);
+      $scope.refreshPuppies();
+    }
+
+    //intializing
+    $scope.refreshPuppies();
   }])
 
 
@@ -32,8 +52,25 @@ puppies.factory("breedService", ['$http', function($http) {
 puppies.factory("puppyService", ['$http', function($http) {
   var puppies = {};
 
+  var _puppies = [];
+
+  puppies.registerNew = function(puppy) {
+    $http({
+          method: "POST",
+          url: "https://ajax-puppies.herokuapp.com/puppies.json",
+          data: puppy
+          });
+  }
+
   puppies.getAll = function() {
     return $http.get("https://ajax-puppies.herokuapp.com/puppies.json");
+  }
+
+  puppies.delete = function(id) {
+    $http({
+      method: "DELETE",
+      url: "https://ajax-puppies.herokuapp.com/puppies/" + id + ".json"
+    })
   }
 
   return puppies;
